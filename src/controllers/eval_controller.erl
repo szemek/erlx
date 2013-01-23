1d350c8a35d4a376039ae6d5772179404cd6ff84
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-01-23 07:51:52 +0100

    better "String" evaluation

--- src/controllers/eval_controller.erl
+++ src/controllers/eval_controller.erl
@@ -7,6 +7,7 @@ index(Req) ->
 
 exec(Req) ->
   PostData = Req:parse_post(),
-  String = proplists:get_value("code", PostData),
-  Eval = lists:flatten(io_lib:write(eval:exec(String))),
+  Code = proplists:get_value("code", PostData),
+  % http://blog.syntaxvssemantics.com/2009/02/convert-arbitrary-erlang-expression-to.html
+  Eval = binary_to_list(list_to_binary(io_lib:format("~p", [eval:exec(Code)]))),
   Req:respond({200, [{"Content-Type", "text/plain"}], Eval}).
